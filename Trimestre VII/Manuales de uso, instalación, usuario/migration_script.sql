93c9927c61d04a97df2c51cd3a9432b770328d6e
jhonelcrack <74622059+jhonelcrack@users.noreply.github.com>
2024-04-04 00:08:30 -0500

    Add files via upload

---
+++ Trimestre VII/Manuales de uso, instalación, usuario/migration_script.sql
@@ -0,0 +1,16 @@
+-- ----------------------------------------------------------------------------
+-- MySQL Workbench Migration
+-- Migrated Schemata: carcesoriosMIGRA
+-- Source Schemata: carcesorios
+-- Created: Wed Apr  3 22:46:27 2024
+-- Workbench Version: 8.0.36
+-- ----------------------------------------------------------------------------
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------------------------------------------------------
+-- Schema carcesoriosMIGRA
+-- ----------------------------------------------------------------------------
+DROP SCHEMA IF EXISTS `carcesoriosMIGRA` ;
+CREATE SCHEMA IF NOT EXISTS `carcesoriosMIGRA` ;
+SET FOREIGN_KEY_CHECKS = 1;
